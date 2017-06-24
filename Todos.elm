4b50444ebaaf9f73e0ef108bb75477a887887c77
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-06-23 23:20:32 -0700

    remove a todo

--- Todos.elm
+++ Todos.elm
@@ -1,6 +1,6 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, input, button, ul, li)
+import Html exposing (Html, text, div, input, button, ul, li, span)
 import Html.Attributes exposing (value, class)
 import Html.Events exposing (onInput, onClick)
 
@@ -10,7 +10,15 @@ view model =
     div [ class "text-center" ]
         [ input [ onInput UpdateText, value model.text ] []
         , button [ onClick AddTodo, class "btn btn-primary" ] [ text "Add" ]
-        , ul [] (List.map (\todo -> li [] [ text todo ]) model.todos)
+        , ul [] (List.indexedMap itemView model.todos)
+        ]
+
+
+itemView : Int -> String -> Html Msg
+itemView index todo =
+    li []
+        [ text todo
+        , span [ onClick (RemoveTodo index) ] [ text " X" ]
         ]
 
 
@@ -21,6 +29,7 @@ type alias Model =
 type Msg
     = UpdateText String
     | AddTodo
+    | RemoveTodo Int
 
 
 update : Msg -> Model -> Model
@@ -31,6 +40,13 @@ update msg model =
 
         AddTodo ->
             { model | text = "", todos = model.text :: model.todos }
+
+        RemoveTodo index ->
+            let
+                newTodos =
+                    (List.take index model.todos) ++ (List.drop (index + 1) model.todos)
+            in
+                { model | todos = newTodos }
 
 
 main : Program Never Model Msg
