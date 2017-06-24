d776e7d80667b61f1a086d7e8daede9f0261b5c3
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-06-23 22:19:42 -0700

    input field with input

--- Todos.elm
+++ Todos.elm
@@ -1,20 +1,34 @@
 module Main exposing (..)
 
-import Html exposing (text, div, input)
+import Html exposing (Html, text, div, input)
 import Html.Attributes exposing (value, class)
+import Html.Events exposing (onInput)
 
 
+view : Model -> Html Msg
 view model =
     div [ class "text-center" ]
-        [ input [ value "Hello, world" ] []
-        , div [] [ text "Hello, world" ]
+        [ input [ onInput UpdateText, value model.text ] []
+        , div [] [ text model.text ]
         ]
 
 
-update model =
-    model
+type alias Model =
+    { text : String }
 
 
+type Msg
+    = UpdateText String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateText newText ->
+            { model | text = newText }
+
+
+main : Program Never Model Msg
 main =
     Html.beginnerProgram
         { view = view
