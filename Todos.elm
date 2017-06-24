2b5122827762a2ea0d02adc7eec1c1cfd4d6cd35
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-06-23 22:57:51 -0700

    add todos

--- Todos.elm
+++ Todos.elm
@@ -1,24 +1,26 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, input)
+import Html exposing (Html, text, div, input, button, ul, li)
 import Html.Attributes exposing (value, class)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 
 
 view : Model -> Html Msg
 view model =
     div [ class "text-center" ]
         [ input [ onInput UpdateText, value model.text ] []
-        , div [] [ text model.text ]
+        , button [ onClick AddTodo, class "btn btn-primary" ] [ text "Add" ]
+        , ul [] (List.map (\todo -> li [] [ text todo ]) model.todos)
         ]
 
 
 type alias Model =
-    { text : String }
+    { text : String, todos : List String }
 
 
 type Msg
     = UpdateText String
+    | AddTodo
 
 
 update : Msg -> Model -> Model
@@ -27,11 +29,14 @@ update msg model =
         UpdateText newText ->
             { model | text = newText }
 
+        AddTodo ->
+            { model | text = "", todos = model.text :: model.todos }
+
 
 main : Program Never Model Msg
 main =
     Html.beginnerProgram
         { view = view
-        , model = { text = "" }
+        , model = { text = "", todos = [] }
         , update = update
         }
