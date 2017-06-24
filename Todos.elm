b016ae611f2b27a54548a33a442a3e899f8a4786
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-06-23 22:08:03 -0700

    input field with nothing

--- Todos.elm
+++ Todos.elm
@@ -1,7 +1,23 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (text, div, input)
+import Html.Attributes exposing (value, class)
+
+
+view model =
+    div [ class "text-center" ]
+        [ input [ value "Hello, world" ] []
+        , div [] [ text "Hello, world" ]
+        ]
+
+
+update model =
+    model
 
 
 main =
-    text "Hello, world"
+    Html.beginnerProgram
+        { view = view
+        , model = { text = "" }
+        , update = update
+        }
